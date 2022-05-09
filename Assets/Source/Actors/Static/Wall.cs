587739489e6750f7e72aadf368ef179e9f726185
Matyi <matyasosv@gmail.com>
2022-05-09 12:04:17 +0200

    init commit, hero cant move through walls

--- Assets/Source/Actors/Static/Wall.cs
+++ Assets/Source/Actors/Static/Wall.cs
@@ -4,5 +4,12 @@
     {
         public override int DefaultSpriteId => 825;
         public override string DefaultName => "Wall";
+
+        public override bool OnCollision(Actor anotherActor)
+        {
+            return false;
+        }
     }
+
+
 }
