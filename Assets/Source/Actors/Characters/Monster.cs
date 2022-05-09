c3a88b95dae9af37d907da625e3e43d34019159c
NemethBalint96 <91986445+NemethBalint96@users.noreply.github.com>
2022-05-09 21:05:14 +0200

    Attacking a monster removes 5 health points. If the health of a monster goes below 0, it dies and disappears. If the hero attacks a monster and it does not die, it also attacks the hero at the same time (it only removes 2 health points).

--- Assets/Source/Actors/Characters/Monster.cs
+++ Assets/Source/Actors/Characters/Monster.cs
@@ -16,5 +16,8 @@ namespace DungeonCrawl.Actors.Characters
 
         public override int DefaultSpriteId => 317;
         public override string DefaultName => "Monster";
+
+        public override int Health { get; set; } = 30;
+        public override int Strength { get; set; } = 3;
     }
 }
--- Assets/Source/Actors/Characters/Skeleton.cs
+++ Assets/Source/Actors/Characters/Skeleton.cs
@@ -9,9 +9,25 @@ namespace DungeonCrawl.Actors.Characters
         public override int DefaultSpriteId => 316;
         public override string DefaultName => "Skeleton";
 
+        public override int Health { get; set; } = 20;
+        public override int Strength { get; set; } = 2;
+
         public override bool OnCollision(Actor anotherActor)
         {
-            return false;
+            if (anotherActor is Player player)
+            {
+                while (player.Health > 0)
+                {
+                    ApplyDamage(player.Strength);
+                    if (this.Health <= 0)
+                    {
+                        break;
+                    }
+                    player.ApplyDamage(this.Strength);
+                }
+            }
+
+            return true;
         }
 
         protected override void OnDeath()
