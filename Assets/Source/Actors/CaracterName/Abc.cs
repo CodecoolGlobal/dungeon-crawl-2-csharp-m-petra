796966bd109a7c7e81bdfa30f602af923e709d5a
NemethBalint96 <91986445+NemethBalint96@users.noreply.github.com>
2022-05-12 12:57:46 +0200

    ascii classes

---
+++ Assets/Source/Actors/CaracterName/Abc.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonCrawl.Actors;
+
+namespace Assets.Source.Actors.CaracterName
+{
+    public class Enter : Actor
+    {
+        public override string DefaultName
+        {
+            get => "!";
+            set { }
+        }
+        public override int DefaultSpriteId => 981;
+    }
+    public class BackSpace : Actor
+    {
+        public override string DefaultName
+        {
+            get => "<";
+            set { }
+        }
+        public override int DefaultSpriteId => 990;
+    }
+    public class Empty : Actor
+    {
+        public override string DefaultName
+        {
+            get => " ";
+            set { }
+        }
+        public override int DefaultSpriteId => 1029;
+    }
+    public class Hyphen : Actor
+    {
+        public override string DefaultName
+        {
+            get => "-";
+            set { }
+        }
+        public override int DefaultSpriteId => 996;
+    }
+    public class Zero : Actor
+    {
+        public override string DefaultName
+        {
+            get => "0";
+            set { }
+        }
+        public override int DefaultSpriteId => 850;
+    }
+    public class One : Actor
+    {
+        public override string DefaultName
+        {
+            get => "1";
+            set { }
+        }
+        public override int DefaultSpriteId => 851;
+    }
+    public class Two : Actor
+    {
+        public override string DefaultName
+        {
+            get => "2";
+            set { }
+        }
+        public override int DefaultSpriteId => 852;
+    }
+    public class Three : Actor
+    {
+        public override string DefaultName
+        {
+            get => "3";
+            set { }
+        }
+        public override int DefaultSpriteId => 853;
+    }
+    public class Four : Actor
+    {
+        public override string DefaultName
+        {
+            get => "4";
+            set { }
+        }
+        public override int DefaultSpriteId => 854;
+    }
+    public class Five : Actor
+    {
+        public override string DefaultName
+        {
+            get => "5";
+            set { }
+        }
+        public override int DefaultSpriteId => 855;
+    }
+    public class Six : Actor
+    {
+        public override string DefaultName
+        {
+            get => "6";
+            set { }
+        }
+        public override int DefaultSpriteId => 856;
+    }
+    public class Seven : Actor
+    {
+        public override string DefaultName
+        {
+            get => "7";
+            set { }
+        }
+        public override int DefaultSpriteId => 857;
+    }
+    public class Eight : Actor
+    {
+        public override string DefaultName
+        {
+            get => "8";
+            set { }
+        }
+        public override int DefaultSpriteId => 858;
+    }
+    public class Nine : Actor
+    {
+        public override string DefaultName
+        {
+            get => "9";
+            set { }
+        }
+        public override int DefaultSpriteId => 859;
+    }
+    public class A : Actor
+    {
+        public override string DefaultName
+        {
+            get => "A";
+            set { }
+        }
+        public override int DefaultSpriteId => 898;
+    }
+    public class B : Actor
+    {
+        public override string DefaultName
+        {
+            get => "B";
+            set { }
+        }
+        public override int DefaultSpriteId => 899;
+    }
+    public class C : Actor
+    {
+        public override string DefaultName
+        {
+            get => "C";
+            set { }
+        }
+        public override int DefaultSpriteId => 900;
+    }
+    public class D : Actor
+    {
+        public override string DefaultName
+        {
+            get => "D";
+            set { }
+        }
+        public override int DefaultSpriteId => 901;
+    }
+    public class E : Actor
+    {
+        public override string DefaultName
+        {
+            get => "E";
+            set { }
+        }
+        public override int DefaultSpriteId => 902;
+    }
+    public class F : Actor
+    {
+        public override string DefaultName
+        {
+            get => "F";
+            set { }
+        }
+        public override int DefaultSpriteId => 903;
+    }
+    public class G : Actor
+    {
+        public override string DefaultName
+        {
+            get => "G";
+            set { }
+        }
+        public override int DefaultSpriteId => 904;
+    }
+    public class H : Actor
+    {
+        public override string DefaultName
+        {
+            get => "H";
+            set { }
+        }
+        public override int DefaultSpriteId => 905;
+    }
+    public class I : Actor
+    {
+        public override string DefaultName
+        {
+            get => "I";
+            set { }
+        }
+        public override int DefaultSpriteId => 906;
+    }
+    public class J : Actor
+    {
+        public override string DefaultName
+        {
+            get => "J";
+            set { }
+        }
+        public override int DefaultSpriteId => 907;
+    }
+    public class K : Actor
+    {
+        public override string DefaultName
+        {
+            get => "K";
+            set { }
+        }
+        public override int DefaultSpriteId => 908;
+    }
+    public class L : Actor
+    {
+        public override string DefaultName
+        {
+            get => "L";
+            set { }
+        }
+        public override int DefaultSpriteId => 909;
+    }
+    public class M : Actor
+    {
+        public override string DefaultName
+        {
+            get => "M";
+            set { }
+        }
+        public override int DefaultSpriteId => 910;
+    }
+    public class N : Actor
+    {
+        public override string DefaultName
+        {
+            get => "N";
+            set { }
+        }
+        public override int DefaultSpriteId => 946;
+    }
+    public class O : Actor
+    {
+        public override string DefaultName
+        {
+            get => "O";
+            set { }
+        }
+        public override int DefaultSpriteId => 947;
+    }
+    public class P : Actor
+    {
+        public override string DefaultName
+        {
+            get => "P";
+            set { }
+        }
+        public override int DefaultSpriteId => 948;
+    }
+    public class Q : Actor
+    {
+        public override string DefaultName
+        {
+            get => "Q";
+            set { }
+        }
+        public override int DefaultSpriteId => 949;
+    }
+    public class R : Actor
+    {
+        public override string DefaultName
+        {
+            get => "R";
+            set { }
+        }
+        public override int DefaultSpriteId => 950;
+    }
+    public class S : Actor
+    {
+        public override string DefaultName
+        {
+            get => "S";
+            set { }
+        }
+        public override int DefaultSpriteId => 951;
+    }
+    public class T : Actor
+    {
+        public override string DefaultName
+        {
+            get => "T";
+            set { }
+        }
+        public override int DefaultSpriteId => 952;
+    }
+    public class U : Actor
+    {
+        public override string DefaultName
+        {
+            get => "U";
+            set { }
+        }
+        public override int DefaultSpriteId => 953;
+    }
+    public class V : Actor
+    {
+        public override string DefaultName
+        {
+            get => "V";
+            set { }
+        }
+        public override int DefaultSpriteId => 954;
+    }
+    public class W : Actor
+    {
+        public override string DefaultName
+        {
+            get => "W";
+            set { }
+        }
+        public override int DefaultSpriteId => 955;
+    }
+    public class X : Actor
+    {
+        public override string DefaultName
+        {
+            get => "X";
+            set { }
+        }
+        public override int DefaultSpriteId => 956;
+    }
+    public class Y : Actor
+    {
+        public override string DefaultName
+        {
+            get => "Y";
+            set { }
+        }
+        public override int DefaultSpriteId => 957;
+    }
+    public class Z : Actor
+    {
+        public override string DefaultName
+        {
+            get => "Z";
+            set { }
+        }
+        public override int DefaultSpriteId => 958;
+    }
+
+}
