46d56e3de8408ab6d30da5310d409bb12e5ad1b3
NemethBalint96 <91986445+NemethBalint96@users.noreply.github.com>
2022-05-12 11:51:08 +0200

    init

---
+++ Assets/Source/Actors/CaracterName/TableItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonCrawl.Actors;
+
+namespace Assets.Source.Actors.Static.Items
+{
+    public class TableItem : Actor
+    {
+        public override string DefaultName
+            {
+                get => "TableItem";
+                set { }
+            }
+
+        public override int DefaultSpriteId => throw new NotImplementedException();
+
+
+    }
+}
--- Assets/Source/Core/GameManager.cs
+++ Assets/Source/Core/GameManager.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Text.RegularExpressions;
+using Assets.Source.Actors.CaracterName;
+using Assets.Source.Actors.Static.Items;
+using UnityEngine;
 
 namespace DungeonCrawl.Core
 {
@@ -9,7 +12,41 @@ namespace DungeonCrawl.Core
     {
         private void Start()
         {
-            MapLoader.LoadMap(1);
+            WelcomeScreen();
+            //MapLoader.LoadMap(1);
+        }
+        private void WelcomeScreen()
+        {
+            ActorManager.Singleton.Spawn<A>((0, 0));
+            CameraController.Singleton.Position = (0, 0);
+            //LoadWelcomeTable();
+            //UserInterface.Singleton.SetText("valami", UserInterface.TextPosition.MiddleCenter);
+
+        }
+        public static void LoadWelcomeTable()
+        {
+            var Chars = Regex.Split(Resources.Load<TextAsset>("characters").text, "\r\n|\r|\n");
+            
+            var heigth = 8;
+            var width = 10;
+
+            for (var y = 0; y < width; y++)
+            {
+                var line = Chars[y + 1];
+                for (var x = 0; x < heigth; x++)
+                {
+                    var character = line[x];
+                    SpawnTable(character, (x, -y));
+                    CameraController.Singleton.Position = (x, -y);
+                }
+            }
+        }
+
+        private static void SpawnTable(char c, (int x, int y) position)
+        {
+            ActorManager.Singleton.Spawn<TableItem>(position);
+
+
         }
     }
 }
