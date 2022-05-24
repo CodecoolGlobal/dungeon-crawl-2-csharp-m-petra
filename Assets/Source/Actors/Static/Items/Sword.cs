936caa882f8eb175453b9649b8559edc62513b13
Kissbence29 <kissbence19@gmail.com>
2022-05-24 18:10:31 +0200

    Swords are added

--- Assets/Source/Actors/Static/Items/Sword.cs
+++ Assets/Source/Actors/Static/Items/Sword.cs
@@ -11,9 +11,29 @@ namespace DungeonCrawl.Actors.Static
         protected override int DefaultSpriteId => 367;
         public override string DefaultName
         {
-            get => "Sword";
+            get => "Widow-maker";
             set { }
         }
         public int Attack => 5;
+    }
+
+    public class Sword1:Weapon
+    {
+        protected override int DefaultSpriteId => 464;
+
+        public override string DefaultName
+        {
+            get=>"Maiden-Tear"; 
+            set{ }
+        }
+    }
+
+    public class Sword2 : Weapon
+    {
+        protected override int DefaultSpriteId => 465;
+        public override string DefaultName { 
+            get=>"Kings-bane";
+            set{ }
+        }
     }
 }
